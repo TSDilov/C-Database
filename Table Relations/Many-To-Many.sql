2a11845b1ec48a012d7f045d350ae16bc466e2a1
Tsvetelin_Dilov <cv.dilov@abv.bg>
2022-05-30 13:56:25 +0300

    Added C# Database course Table Relations SQL

---
+++ Table Relations/Many-To-Many.sql
@@ -0,0 +1,46 @@
+CREATE TABLE Students
+(
+	StudentID INT IDENTITY PRIMARY KEY,
+	Name NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Exams
+(
+	ExamID INT IDENTITY(101,1) PRIMARY KEY,
+	Name NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE StudentsExams
+(
+	StudentID INT,
+	ExamID INT,
+	CONSTRAINT PK_StudentsExams
+	PRIMARY KEY (StudentID, ExamID),
+	CONSTRAINT FK_StudentsExams_Students
+	FOREIGN KEY (StudentID)
+	REFERENCES Students(StudentID),
+	CONSTRAINT FK_StudentsExams_Exams
+	FOREIGN KEY (ExamID)
+	REFERENCES ExamS(ExamID)
+)
+
+INSERT INTO Students(Name)
+	VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+INSERT INTO Exams(Name)
+	VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO StudentsExams(StudentID, ExamID)
+	VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103)
