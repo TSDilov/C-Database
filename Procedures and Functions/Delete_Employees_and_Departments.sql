3509e94c9b689382598b7906df94619ca2903210
Tsvetelin_Dilov <cv.dilov@abv.bg>
2022-06-13 10:30:17 +0300

    Added C# Database course SQL

---
+++ Procedures and Functions/Delete_Employees_and_Departments.sql
@@ -0,0 +1,41 @@
+CREATE OR ALTER PROC usp_DeleteEmployeesFromDepartment(@DepartmentId INT) 
+AS
+	ALTER TABLE Departments
+	ALTER COLUMN ManagerID INT NULL
+
+	DELETE 
+	FROM EmployeesProjects
+	WHERE EmployeeID IN (SELECT EmployeeID
+						FROM Employees
+						WHERE DepartmentID = @DepartmentId)
+
+	UPDATE Employees
+		SET ManagerID = NULL
+	WHERE EmployeeID IN (SELECT EmployeeID
+						FROM Employees
+						WHERE DepartmentID = @DepartmentId)
+
+	UPDATE Employees
+		SET ManagerID = NULL
+	WHERE ManagerID IN (SELECT EmployeeID
+						FROM Employees
+						WHERE DepartmentID = @DepartmentId)
+
+	UPDATE Departments
+		SET ManagerID = NULL
+	WHERE DepartmentID = @DepartmentId
+
+	DELETE 
+	FROM Employees
+	WHERE DepartmentID = @DepartmentId
+
+	DELETE
+	FROM Departments
+	WHERE DepartmentID = @DepartmentId
+
+	SELECT COUNT(*)
+	FROM Employees
+	WHERE DepartmentID = @DepartmentId
+
+EXEC usp_DeleteEmployeesFromDepartment 3
+
