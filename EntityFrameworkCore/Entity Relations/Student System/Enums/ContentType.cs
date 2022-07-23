10b3a5304012d44b9a55093724e72f00df56b56d
Tsvetelin91 <cv.dilov@abv.bg>
2022-07-23 11:37:01 +0300

    Added Enums

---
+++ EntityFrameworkCore/Entity Relations/Student System/Enums/ContentType.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentSystem.Enums
+{
+    public enum ContentType
+    {
+        Application = 1,
+        Pdf = 2,
+        Zip = 3
+    }
+}
---
+++ EntityFrameworkCore/Entity Relations/Student System/Enums/ResourseType.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentSystem.Enums
+{
+    public enum ResourseType
+    {
+        Video = 1,
+        Presentation = 2,
+        Document = 3,
+        Other = 4
+    }
+}
