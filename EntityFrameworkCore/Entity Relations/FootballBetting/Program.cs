8a094034e6b828e65230d612642da655b280ca7c
Tsvetelin91 <cv.dilov@abv.bg>
2022-07-28 12:06:35 +0300

    Added StartUp

---
+++ EntityFrameworkCore/Entity Relations/FootballBetting/Program.cs
@@ -0,0 +1,14 @@
+﻿using FootballBetting.Data;
+using System;
+
+namespace FootballBetting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new FootballBettingContext();
+            context.Database.EnsureCreated();
+        }
+    }
+}
