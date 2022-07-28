8637e8d6e246eec815f7e7cb169242f4583253d6
Tsvetelin91 <cv.dilov@abv.bg>
2022-07-28 12:05:24 +0300

    Added Context for the base

---
+++ EntityFrameworkCore/Entity Relations/FootballBetting/Data/FootballBettingContext.cs
@@ -0,0 +1,50 @@
+﻿using FootballBetting.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballBetting.Data
+{
+    public class FootballBettingContext : DbContext
+    {
+        public FootballBettingContext()
+        {
+
+        }
+
+        public FootballBettingContext(DbContextOptions<FootballBettingContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<Bet> Bets { get; set; }
+        public virtual DbSet<Color> Colors { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Game> Games { get; set; }
+        public virtual DbSet<Player> Players { get; set; }
+        public virtual DbSet<PlayerStatistics> PlayersStatistic { get; set; }
+        public virtual DbSet<Position> Positions { get; set; }
+        public virtual DbSet<Team> Teams { get; set; }
+        public virtual DbSet<Town> Towns { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=FootballBetting;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayerStatistics>()
+                .HasKey(k => new { k.PlayertId, k.GameId });
+        }
+    }
+}
