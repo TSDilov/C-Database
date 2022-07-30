5a537bd73c3c3c8e516cc56f192c8c1f9f220d71
Tsvetelin91 <cv.dilov@abv.bg>
2022-07-30 09:46:01 +0300

    Added Context and Models

---
+++ EntityFrameworkCore/Entity Relations/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        public Album()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+        
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [NotMapped]
+        public decimal Price { get; set; }
+
+        public int ProducerId { get; set; }
+        public Producer Producer { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+
+    }
+}
