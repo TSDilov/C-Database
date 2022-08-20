b4c17d781ddedba6e2f210da9a0ee283c0bc4b72
Tsvetelin91 <cv.dilov@abv.bg>
2022-08-20 10:29:35 +0300

    Added DbContext

---
+++ EntityFrameworkCore/SoftUni/Data/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUni.Data
+{
+    public class Configuration
+    {
+        public static string ConnectionString
+            => "Server=.;Database=BookShop;Integrated Security=True;";
+    }
+}
