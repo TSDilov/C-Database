163df36516d2cb63e3c4326a8fd93ab987c61e4c
Tsvetelin91 <cv.dilov@abv.bg>
2022-08-21 16:25:12 +0300

    Added profile for automapper

---
+++ EntityFrameworkCore/Json Processing/Car Dealer/CarDealerProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using CarDealer.AppModels;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<ImportSupplier, Supplier>();
+
+            this.CreateMap<ImportPart, Part>();
+
+            this.CreateMap<ImportCar, Car>();
+        }
+    }
+}
