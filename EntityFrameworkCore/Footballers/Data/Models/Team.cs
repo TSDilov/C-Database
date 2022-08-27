0d82e93b33a7d99a4b7bcb251a68c38375f137ff
Tsvetelin91 <cv.dilov@abv.bg>
2022-08-27 14:46:16 +0300

    Added dbContext and models for the base

---
+++ EntityFrameworkCore/Footballers/Data/Models/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Footballers.Data.Models
+{
+    public class Team
+    {
+        public Team()
+        {
+            this.TeamsFootballers = new HashSet<TeamFootballer>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Nationality  { get; set; }
+
+        public int Trophies  { get; set; }
+
+        public ICollection<TeamFootballer> TeamsFootballers  { get; set; }
+    }
+}
